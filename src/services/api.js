bc1f6913d0fc447873aaa93edb390196b0a2a5d4
Stepaniuk-Den <stepaniuk.den@gmail.com>
2023-08-16 07:43:53 +0300

    create

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const $instance = axios.create({
+  baseURL: 'https://64c21d6bfa35860baea1349e.mockapi.io/',
+});
+
+export const fetchCar = async () => {
+  const { data } = await $instance.get('/adverts');
+  // console.log(data)
+  return data;
+}
