e9e19c6ba3b3a9f16e90e32014dd41c798efde34
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-02-14 21:05:51 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
+    <BrowserRouter basename="/RentalCar">
       <PersistGate loading={null} persistor={persistor}>
-        {/* <BrowserRouter basename="/RentalCar"> */}
         <App />
       </PersistGate>
     </BrowserRouter>
